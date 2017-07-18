f15f3e77b57f36faa7a227fb67f09bb5a5c8ffdb
samilily27 <samilily27@yahoo.com>
2017-07-18 14:25:13 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,36 @@
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+
+$mail_to = 'test@test-mail.com';
+
+$subject = 'Message from a site visitor ' . $field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = "From: $cf_email\r\n";
+$headers .= "Reply-To: $cf_email\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+    <script language="javascript" type="text/javascript">
+        // Print a message
+        alert('Thank you for the message. We will contact you shortly.');
+        // Redirect to some page of the site. You can also specify full URL, e.g. http://template-help.com
+        window.location = 'contact.html';
+    </script>
+<?php
+}
+
+else { ?>
+    <script language="javascript" type="text/javascript">
+        // Print a message
+        alert('Message failed. Please, send an email to gordon@template-help.com');
+        // Redirect to some page of the site. You can also specify full URL, e.g. http://template-help.com
+        window.location = 'contact.html';
+    </script>
+<?php
+}?>
